8912cfc36f1f802fa64d268a6df16e464d058c95
Laura Messa Travin <laura.travin@gmail.com>
2021-03-21 21:08:54 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210321205500) do
+ActiveRecord::Schema.define(version: 20210321210718) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20210321205500) do
   create_table "rides", force: :cascade do |t|
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "passanger_id"
+    t.integer  "passenger_id"
     t.integer  "taxi_id"
   end
 
